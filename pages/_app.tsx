6853a44488cdfb45a427cdf6aca0d4e66214a751
Pedro Britto <pedro.britto@oowlish.com>
2021-02-20 11:26:53 -0300

    Add absolute paths to tsconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import Head from "next/head";
 import "normalize.css";
-import ThemeProvider from "../theme/index.js";
+
+import ThemeProvider from "@/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider>
         <Component {...pageProps} />
